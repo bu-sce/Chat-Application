ea7887d336483824ef4354f0eff781362a5b64ae
Diaa Hassan <diaa180409@feng.bu.edu.eg>
2022-08-10 01:15:02 +0200

    removed builds

--- server/build/routes/root.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// const express = require('express');
-// const router = express.Router();
-// import { Request, Response } from 'express';
-// router.get('^/$|/index(.html)?', (req : Request, res : Response) => {
-//     res.json({"message" : "hello"});
-// });
-// module.exports = router;
