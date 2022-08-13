e7845ed36598da93c3ba26250a737fe6a584b5a7
karim moftah <karim180547@feng.bu.edu.eg>
2022-08-14 00:56:26 +0200

    update User Model

--- server/src/models/userModel.ts
+++ server/src/models/userModel.ts
@@ -1,6 +1,10 @@
-const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+import mongoose, { Document, Schema  } from 'mongoose';
+const crypto = require("crypto");
+const bcrypt = require('bcrypt');
+
+
+const userSchema : Schema= new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -29,6 +33,37 @@ const userSchema = new mongoose.Schema({
   },
   refreshToken: String,
   googleId : String,
+  resetPasswordToken : String,
+  resetPasswordExpire : Date,
+
 });
 
-export default mongoose.model("User", userSchema);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.getResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  // Hash token (private key) and save to database
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  // Set token expire date
+  this.resetPasswordExpire = Date.now() + 10 * (60 * 1000); // Ten Minutes
+
+  return resetToken;
+};
+
+
+const user = mongoose.model("User", userSchema);
+
+export default user
