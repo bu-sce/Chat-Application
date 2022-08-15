6c50b4b6836cdef77c8cd2597814d28776c0b8df
ammaradel17 <amar170518@feng.bu.edu.eg>
2022-08-15 15:59:51 +0200

    Make The Chat Page Responsive

--- public/src/components/Contacts.jsx
+++ public/src/components/Contacts.jsx
@@ -80,176 +80,123 @@ export default function Contacts({ contacts, changeChat }) {
   )
 }
 const Container = styled.div`
-  display: grid;
-  grid-template-rows: 10% 75% 15%;
-  box-shadow: 2px 2px 2px 1px #20272e;
-  border-radius: 2rem;
-  overflow: hidden;
-  background-color: #f5f5f5;
-  .brand {
-    background-color: white;
-    margin-bottom: 0px;
-    padding: 10px;
-    display: flex;
-    align-items: center;
-    gap: 1rem;
-    justify-content: center;
-    h3 {
-      color: #29abff;
-      font-family: 'Pacifico', cursive;
-      font-size: 25px;
-    }
+display: grid;
+grid-template-rows: 10% 75% 15%;
+box-shadow: 2px 2px 2px 1px #20272e;
+border-radius: 2rem;
+overflow: hidden;
+background-color: #f5f5f5;
+.brand {
+  background-color: white;
+  margin-bottom: 0px;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  justify-content: center;
+  h3 {
+    color: #29abff;
+    font-family: 'Pacifico', cursive;
+    font-size: 25px;
   }
-  span {
-    color: #20272e;
-  }
-  .contacts {
-    display: flex;
-    padding-top: 10px;
-    flex-direction: column;
-    align-items: center;
-    overflow: auto;
-    gap: 0.8rem;
-    &::-webkit-scrollbar {
-      width: 0.4rem;
-      &-thumb {
-        background-color: #29abff;
-        width: 0.1rem;
-        border-radius: 1rem;
-      }
-    }
-    .contact {
-      background-color: #f5f5f5;
-      min-height: 3.4rem;
-      cursor: pointer;
-      width: 90%;
-      border-radius: 1rem;
-      padding-top: 0.4rem;
-      padding-left: 0.4rem;
-      display: flex;
-      gap: 1rem;
-      align-items: center;
-      transition: 0.3s ease-in-out;
-      .avatar {
-        img {
-          height: 2.8rem;
-        }
-      }
-      .username {
-        h3 {
-          color: #20272e;
-          text-transform: capitalize;
-        }
-      }
-      &:hover {
-        background-color: white;
-      }
-    }
-    .selected {
+}
+span {
+  color: #20272e;
+}
+.contacts {
+  display: flex;
+  padding-top: 10px;
+  flex-direction: column;
+  align-items: center;
+  overflow: auto;
+  gap: 0.8rem;
+  &::-webkit-scrollbar {
+    width: 0.4rem;
+    &-thumb {
       background-color: #29abff;
-      .username {
-        h3 {
-          color: #fff;
-          text-transform: capitalize;
-        }
-      }
-      &:hover {
-        background-color: #fff;
-        .username {
-          h3 {
-            color: #29abff;
-          }
-        }
-      }
+      width: 0.1rem;
+      border-radius: 1rem;
     }
   }
-  .current-user {
-    background-color: #20272e;
-    background-color: rgba(0, 0, 0, 0.8);
+  .contact {
+    background-color: #f5f5f5;
+    min-height: 3.4rem;
+    cursor: pointer;
+    width: 90%;
+    border-radius: 1rem;
+    padding-top: 0.4rem;
+    padding-left: 0.4rem;
     display: flex;
-    justify-content: center;
-    align-items: center;
     gap: 1rem;
+    align-items: center;
+    transition: 0.3s ease-in-out;
     .avatar {
-      cursor: pointer;
-      display: inline-block;
-      padding-top: 10px;
-      position: relative;
-
-      .dropup-content {
-        display: none;
-        position: absolute;
-        background-color: #29abff;
-        // background-color: rgba(0, 0, 0, 0.8);
-        // box-shadow: 2px 2px 2px 1px #29abff;
-        border-radius: 17px;
-        // width: 80%;
-        padding-left: 10px;
-        padding-right: 10px;
-        height: 50%;
-        margin: 8px;
-        bottom: 60px;
-        z-index: 1;
-        &:before {
-          content: '';
-          position: absolute;
-          bottom: 100%;
-          top: 36px;
-          left: 28px;
-          width: 0px;
-          height: 0px;
-          border-left: 20px solid #29abff;
-          border-bottom: 12px solid transparent;
-        }
-      }
-      .dropup-content button,
-      Logout {
-        background-color: #29abff;
-        // background-color: rgba(0, 0, 0, 0.8);
-        // padding-top: 10px;
-        text-decoration: none;
-        display: block;
-
-        border-radius: 17px;
-      }
-
-      .dropup-content button:hover {
-        // background-color: #ddd;
-      }
-      &:hover .dropup-content {
-        display: flex;
-
-        justify-contents: space-between;
-        align-items: center;
-        &:after {
-          border-bottom-color: $blue;
-          left: 93%;
-        }
-      }
       img {
-        height: 4rem;
-        max-inline-size: 100%;
-        display: block;
-      }
-      button {
-        border: 0;
-      }
-      svg {
-        background-color: transparent;
-        color: #fff;
-        font-size: 1.2rem;
-        cursor: pointer;
-      }
-      &:after {
-        border-bottom-color: $blue;
-        left: 93%;
+        height: 2.8rem;
       }
     }
     .username {
-      h2 {
-        color: white;
+      h3 {
+        color: #20272e;
         text-transform: capitalize;
       }
     }
+    &:hover {
+      background-color: white;
+    }
   }
+  .selected {
+    background-color: #29abff;
+    .username {
+      h3 {
+        color: #fff;
+        text-transform: capitalize;
+      }
+    }
+    &:hover {
+      background-color: #fff;
+      .username {
+        h3 {
+          color: #29abff;
+        }
+      }
+    }
+  }
+}
+.current-user {
+  background-color: #20272e;
+  background-color: rgba(0, 0, 0, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  .avatar {
+    position: relative;
+    padding-top: 10px;
+    img {
+      height: 4rem;
+      max-inline-size: 100%;
+      display: block;
+    }
+    button {
+      padding: 0;
+      border: 0;
+    }
+    svg {
+      position: absolute;
+      left: 0;
+      top: 10px;
+      background-color: transparent;
+      color: #fff;
+      font-size: 1.2rem;
+      cursor: pointer;
+    }
+  }
+  .username {
+    h2 {
+      color: white;
+      text-transform: capitalize;
+    }
+  }
+}
 `
--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -8,12 +8,43 @@ import ChatContainer from '../components/ChatContainer'
 import Contacts from '../components/Contacts'
 import Welcome from '../components/Welcome'
 import bg from '../assets/bg.jpg'
+import {MdOutlineKeyboardBackspace} from 'react-icons/md'
 export default function Chat() {
   const navigate = useNavigate()
   const socket = useRef()
   const [contacts, setContacts] = useState([])
   const [currentChat, setCurrentChat] = useState(undefined)
   const [currentUser, setCurrentUser] = useState(undefined)
+  const [toggle, setToggle] = useState(true)
+  let f 
+  function getWindowDimensions() {
+    const { innerWidth: width} = window;
+    return {
+      width
+    };
+  }
+  let u =()=>{
+    f=getWindowDimensions().width
+    if(f<960){
+      return true
+    }else{
+      return false
+    }
+  }
+  const [res,setRes]=useState(u)
+  
+  window.addEventListener('resize', function(){
+    f =getWindowDimensions()
+    if(f.width<960){
+      setRes(true)
+    }else{
+      setRes(false)
+    }
+  });
+  
+  const handleToggle=()=>{
+    setToggle(true)
+  }
   useEffect(() => {
     const a = async () => {
       if (!localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
@@ -49,19 +80,33 @@ export default function Chat() {
   }, [currentUser])
   const handleChatChange = (chat) => {
     setCurrentChat(chat)
+    setToggle(false)
   }
   return (
     <>
       <Container>
         <div
           className="container"
-          // ${currentChat === undefined ? '' : 'chat'}`}
         >
-          <Contacts contacts={contacts} changeChat={handleChatChange} />
-          {currentChat === undefined ? (
-            <Welcome />
-          ) : (
-            <ChatContainer currentChat={currentChat} socket={socket} />
+          {res === false ?(
+           <>
+            <Contacts contacts={contacts} changeChat={handleChatChange} />
+            {currentChat === undefined ? (
+              <Welcome />
+            ) : (
+              <ChatContainer  currentChat={currentChat} socket={socket}  />
+            )}</>
+          ):(
+            <>
+              {toggle === true ? (
+                   <Contacts contacts={contacts} changeChat={handleChatChange}  />
+              ):(
+                <>
+                <button className='button' onClick={handleToggle}><MdOutlineKeyboardBackspace /></button>
+                <ChatContainer  currentChat={currentChat} socket={socket}  />
+                </>
+              )}
+            </>
           )}
         </div>
       </Container>
@@ -70,26 +115,48 @@ export default function Chat() {
 }
 
 const Container = styled.div`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 1rem;
-  align-items: center;
+height: 100vh;
+width: 100vw;
+display: flex;
+flex-direction: column;
+justify-content: center;
+gap: 1rem;
+align-items: center;
+box-shadow: 2px 2px 2px 1px #20272e;
+background-color: white;
+background-image: url("${bg}");
+
+.container {
+  border-radius: 5rem;
+  height: 90vh;
+  width: 90vw;
+  background-color: #00000076;
   box-shadow: 2px 2px 2px 1px #20272e;
-  background-color: white;
-  background-image: url("${bg}");
-  .container {
-    border-radius: 5rem;
-    height: 90vh;
-    width: 90vw;
-    background-color: #00000076;
-    box-shadow: 2px 2px 2px 1px #20272e;
-    display: grid;
-    grid-template-columns: 30% 70%;
+  display: grid;
+  grid-template-columns: 30% 70% ;
+  border-radius: 2rem;
+  .button{
+    position:absolute; 
+    display:none;
   }
-  .chat {
-    background-color: #f5ffffaf;
+  @media screen and (max-width: 959px) {
+      grid-template-columns: 100% 0% !important;
+      .button{
+        padding: 0.3rem 2rem;
+        border-radius: 2rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: transparent;
+        border: none;
+        svg {
+          font-size: 2rem;
+          color: #20272e;
+        } 
+      } 
   }
+}
+.chat {
+  background-color: #f5ffffaf;
+}
 `
